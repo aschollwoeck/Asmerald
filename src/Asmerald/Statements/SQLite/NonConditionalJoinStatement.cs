dbaf9cecb50dd22d06acabf2d8b562e5fab5b4dc
aschollwoeck <a.schollwoeck@outlook.com>
2022-12-23 15:40:47 +0100

    Changed base to Join class instead of ConditionalJoin

--- src/Asmerald/Statements/SQLite/NonConditionalJoinStatement.cs
+++ src/Asmerald/Statements/SQLite/NonConditionalJoinStatement.cs
@@ -5,16 +5,12 @@ using Asmerald.Expressions;
 
 namespace Asmerald.Statements.SQLite
 {
-    public class NonConditionalJoinStatement : ConditionalJoinStatement
+    public class NonConditionalJoinStatement : JoinStatement
     {
-        public ITable Left { get; private set; }
-        public ITable Right { get; private set; }
         public NonConditionalJoinStatement() { }
         public NonConditionalJoinStatement(IQueryBuilder queryBuilder, ITable left, ITable right)
             : base(queryBuilder, left, right)
         {
-            this.Left = left;
-            this.Right = right;
         }
     }
 }
